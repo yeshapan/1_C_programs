5bcd44041980d88d51a1301ed3f55aa550489fe4
Yesha Pandya <89733331+yeshapan@users.noreply.github.com>
2023-12-31 20:22:05 +0530

    Create 43_addition_using_pointers.c

---
+++ 43_addition_using_pointers.c
@@ -0,0 +1,24 @@
+//C program to add two numbers using pointers
+
+#include <stdio.h>
+
+int main() {
+    // Declare variables
+    int num1, num2, sum;
+
+    // Input two numbers from the user
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Add numbers using pointers
+    *sum = *num1 + *num2
+
+    // Display result
+    printf("Sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
+
