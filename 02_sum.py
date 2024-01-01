7ec315bec26d391bf98d1ebf4366ee0e35b24189
Yesha Pandya <89733331+yeshapan@users.noreply.github.com>
2024-01-01 21:06:13 +0530

    Create 02_sum.py

---
+++ 02_sum.py
@@ -0,0 +1,9 @@
+#Python program to find sum of two numbers
+
+#take numbers from user
+num1= float(input("Enter first number: "));
+num2= float(input("Enter second number: "));
+
+#calculate and print sum
+sum= num1 + num2;
+print("Sum of {num1} and {num2} is: {sum}");
