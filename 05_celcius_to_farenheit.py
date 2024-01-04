9a9448c3010b0015decdfb538e0d3a210aecb1d4
Yesha Pandya <yesha.pandya@outlook.com>
2024-01-04 11:18:12 +0530

    Create 05_celcius_to_farenheit.py

---
+++ 05_celcius_to_farenheit.py
@@ -0,0 +1,8 @@
+#Python program to convert temperature in degree celcius to farenheit
+
+#take input temp in degree celcius
+cel=float(input("Enter temperature in degree celcuis: "))
+
+#covert to farenheit and print
+faren = (cel*9/5) + 32
+print("Temperature in degree farenheit is: ", faren)
