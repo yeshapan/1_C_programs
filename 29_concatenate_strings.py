fdec811a8ebf09052fb94ba11d6293c46fc81db6
Yesha Pandya <yesha.pandya@outlook.com>
2024-01-04 17:33:50 +0530

    Create 29_concatenate_strings.py

---
+++ 29_concatenate_strings.py
@@ -0,0 +1,9 @@
+#Python program to concatenate two strings
+
+#take input strings from user
+str1=input("Enter first string: ")
+str2=input("Enter second string: ")
+
+#concatenate strings and print result
+concat_str= str1 + str2
+print(f"Concatenated string is: {concat_str}")
