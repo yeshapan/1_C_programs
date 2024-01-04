5ff8dab504890e0e986dcaf8594ab6660474c39b
Yesha Pandya <yesha.pandya@outlook.com>
2024-01-04 14:56:12 +0530

    Create 11_greatest_num.py

---
+++ 11_greatest_num.py
@@ -0,0 +1,10 @@
+# Python program to find greatest among three numbers
+
+#input numbers
+num1= float(input("Enter first number: "))
+num2= float(input("Enter second number: "))
+num3= float(input("Enter third number: "))
+
+#find and print greatest number
+max_num= max(num1, num2, num3)   #used max inbuilt function
+print("Greatest number is: ", max_num)
